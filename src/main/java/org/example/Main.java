21f9c757535571912fa896bfdc220e6c29b4a83d
Nast_programmer <anastypol33@gmail.com>
2024-06-20 22:36:33 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,68 @@
+package org.example;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+        HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
+        CloseableHttpResponse response = httpClient.execute(request);
+        List<Post> list = new ObjectMapper().readValue(response.getEntity().getContent(), new TypeReference<>() {});
+        list.stream().filter(x -> x.getUpvotes() != null && x.getUpvotes() > 0).forEach(System.out::println);
+    }
+}
+
+class Post {
+
+    private String id;
+    private String text;
+    private String type;
+    private String user;
+    private Integer upvotes;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public Integer getUpvotes() {
+        return upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return "{ \n" +
+                "id: \"" + id + "\"," + "\n" +
+                "text: \"" + text + "\"," + "\n" +
+                "type: \"" + type + "\"," + "\n" +
+                "user: \"" + user + "\"," + "\n" +
+                "upvotes: \"" + upvotes + "\"," + "\n" +
+                '}';
+    }
+}
